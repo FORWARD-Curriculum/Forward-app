c9beb61a21711e2d49453d7e9ff5fc931726a79d
varphi-online <nbash1984@gmail.com>
2025-02-19 15:47:36 -0700

    Fixed: undefined field "detail" Bug

--- Forward-client/app/routes/register.tsx
+++ Forward-client/app/routes/register.tsx
@@ -47,15 +47,23 @@ export default function Login() {
       const result = await response.json();
 
       if (!response.ok) {
-        // Handle field-specific errors
-        if (result.detail && typeof result.detail === "object") {
-          const errorMessages = Object.values(result.detail)
-            .map(messages => (messages as string[]).join("\n"))
-            .join("\n");
-          throw new Error(errorMessages);
+       
+        if (result.detail) {
+          if (typeof result.detail === "object") {
+             // Handle field-specific errors
+            const errorMessages = Object.values(result.detail)
+              .map((messages) => (messages as string[]).join("\n"))
+              .join("\n");
+            throw new Error(errorMessages);
+          } else {
+            // Handle simple string errors
+            throw new Error(result.detail);
+          }
+          
         }
-        // Handle simple string errors
-        throw new Error(result.detail);
+        // Handle invalid server responses / Server non-responses
+        toast.error("Registration failed. Please try again.")
+        throw new Error("Registration failed. Please try again.")
       }
 
       const user: User = {
