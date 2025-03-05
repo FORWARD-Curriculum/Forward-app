ab905508922a9d36b227f9f04989afff83ac5ff5
varphi-online <nbash1984@gmail.com>
2025-03-05 13:15:04 -0700

    fixed several bugs having to do with new image selection

--- Forward-client/app/routes/account.tsx
+++ Forward-client/app/routes/account.tsx
@@ -113,7 +113,7 @@ export default function account() {
      * niceties.
      */
     const data = {
-      profile_picture: formState.profilePic || formState.removedPicture ? null:user.profile_picture,
+      profile_picture: formState.removedPicture ? null:(formState.profilePic??user.profile_picture??null),
       display_name: formData.get("display_name"),
       consent: formData.has("consent"),
       theme: formState.theme,
@@ -168,14 +168,9 @@ export default function account() {
             </div>
             <div className="flex flex-col items-center gap-1">
               <div
-                className={`flex h-30 w-30 items-center justify-center overflow-hidden rounded-full ${
-                  formState.profilePic ||
-                  (user.profile_picture && !formState.removedPicture)
-                    ? ""
-                    : "border-muted-foreground border-1 border-solid"
-                }`}
+                className={`flex h-30 w-30 items-center justify-center overflow-hidden rounded-full ${!((formState.profilePic && !formState.removedPicture) || (user.profile_picture && !formState.removedPicture)) && "border-muted-foreground border-1 border-solid"}`}
               >
-                {formState.profilePic ||
+                {(formState.profilePic && !formState.removedPicture) ||
                 (user.profile_picture && !formState.removedPicture) ? (
                   <img
                     src={formState.profilePic || user.profile_picture}
@@ -200,21 +195,22 @@ export default function account() {
                       user?.preferences?.theme ||
                       "" + user?.preferences?.text_size ||
                       ""
-                    } bg-foreground border-secondary-foreground w-fit flex flex-col items-center align-middle text-secondary-foreground`}
+                    } bg-foreground border-secondary-foreground text-secondary-foreground flex w-fit flex-col items-center align-middle`}
                   >
-                    <h2 className="text-wrap w-fit">
+                    <h2 className="w-fit text-wrap">
                       Select new Profile Picture
                     </h2>
-                    <div className=" grid grid-cols-3 justify-items-center items-center grid-rows-3 w-fit">
+                    <div className="grid w-fit grid-cols-3 grid-rows-3 items-center justify-items-center">
                       {Array(9)
                         .fill(0)
                         .map((_, i) => (
                           <PopoverClose
-                            className="overflow-hidden h-15 rounded-full m-1"
+                            className="active:outline-secondary-foreground hover:outline-muted-foreground m-1 h-15 overflow-hidden rounded-full outline-offset-2 hover:outline-2 active:outline-2"
                             onClick={() => {
                               setFormState({
                                 ...formState,
                                 profilePic: `/profile_pictures/${i + 1}.jpg`,
+                                removedPicture: false,
                               });
                             }}
                           >
