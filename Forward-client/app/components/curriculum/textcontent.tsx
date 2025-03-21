bcc7c0650261a7b3041a0a82754d0e5c330d1b8a
varphi-online <nbash1984@gmail.com>
2025-03-20 17:33:47 -0700

    markdown  support in text-content

--- Forward-client/app/components/curriculum/textcontent.tsx
+++ Forward-client/app/components/curriculum/textcontent.tsx
@@ -1,9 +1,11 @@
 import type { TextContent } from "@/lib/lessonSlice";
+import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 
 export default function TextContent({textContent}: {textContent: TextContent}) {
     return (
-        <div>
-        <p>{textContent.content}</p>
+        <div className="markdown">
+        <Markdown rehypePlugins={[rehypeRaw]}>{textContent.content}</Markdown>
         </div>
     );
 }
