e93cac321567b0c060a82fbc25fcca3c1af8c2ac
Low-go <lorranalvesgaldino98@gmail.com>
2025-08-01 17:28:36 -1000

    Script seeds minio if no bucket or if bucket size does not match asset folder. Urls created

--- Forward-server/core/management/commands/seed_minIO_data.py
+++ Forward-server/core/management/commands/seed_minIO_data.py
@@ -2,18 +2,40 @@ from django.core.management.base import BaseCommand
 from pathlib import Path
 from django.conf import settings
 from django.core.files.storage import default_storage
-from botocore.exceptions import ClientError
-import boto3
+from botocore.exceptions import ClientError # pyright: ignore[reportMissingImports]
+import boto3 # pyright: ignore[reportMissingImports]
 
 class Command(BaseCommand):
-
-
     
+    def upload_files_to_bucket(self, seed_minIO_folder):
+        for file_path in seed_minIO_folder.iterdir():
+            filename = Path(file_path).name
+            with open(file_path, 'rb') as f:
+                saved_path = default_storage.save(f'{filename}', f)
+                # might need this
+                url = default_storage.url(saved_path)
+                self.stdout.write(self.style.SUCCESS(f'Image uploaded, url: {url}'))
+   
     def handle(self, *args, **options):
-
+        seed_minIO_folder = Path(settings.BASE_DIR) / 'core' / 'management' / 'minIO_asset_seed'
         try:
             files = default_storage.listdir('')
-            print("Bucket exists and accessible!")
+            bucket_file_count = len(files)
+            
+            """
+                This is temporary. If a new media was added and the
+                amount dosent match with those in minio volume
+                its overwritten with new contents.
+                Maybe we can make it just add the new ones in the future?
+            """
+            local_files = list(seed_minIO_folder.iterdir())
+            local_file_count = len([f for f in local_files if f.is_file()])
+            if bucket_file_count == local_file_count:
+                self.stdout.write(self.style.SUCCESS('Bucket exists'))
+                return
+            else:
+                self.upload_files_to_bucket(seed_minIO_folder)
+                
         except ClientError:
             self.stdout.write(self.style.ERROR('No bucket found.'))
             self.stdout.write(self.style.ERROR('Creating bucket'))
@@ -26,7 +48,9 @@ class Command(BaseCommand):
                 aws_secret_access_key='minioadmin'  
             )
 
-            bucket_name = settings.STORAGES['default']['OPTIONS']['bucket_name']  # or get it from your settings
+            bucket_name = settings.STORAGES['default']['OPTIONS']['bucket_name']
             s3_client.create_bucket(Bucket=bucket_name)
-
             self.stdout.write(self.style.SUCCESS(f'Bucket Created: {bucket_name}'))
+            
+            # One time seed to minio volume of existing image files
+            self.upload_files_to_bucket(seed_minIO_folder)
