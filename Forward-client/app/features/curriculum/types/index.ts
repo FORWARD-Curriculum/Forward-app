0507cb2bb6ae7031d71bb7e8d915b831c177b76d
Low-go <lorranalvesgaldino98@gmail.com>
2025-11-10 23:34:03 -1000

    image files for quiz and question successfully display, they are optional fields

--- Forward-client/app/features/curriculum/types/index.ts
+++ Forward-client/app/features/curriculum/types/index.ts
@@ -120,6 +120,7 @@ export interface Quiz extends BaseActivity {
     failing: string;
   };
   questions: Question[];
+  image?: string;
 }
 
 export interface Question {
@@ -137,6 +138,7 @@ export interface Question {
       text: string;
       is_correct: boolean;
     }[];
+    image?: string;
   };
   is_required: boolean;
   attempts?: number;
