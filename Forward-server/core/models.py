4197f0475fa716471188865235b46aba41b00240
Low-go <lorranalvesgaldino98@gmail.com>
2025-11-13 19:32:22 -1000

    lesson 4 progress halfway complete minor corrections in json

--- Forward-server/core/models.py
+++ Forward-server/core/models.py
@@ -1033,6 +1033,7 @@ class ConceptMap(BaseActivity):
         verbose_name = "Concept Map"
         verbose_name_plural = "Concept Maps"
 
+    #TODO return and configure images for fill in the blank activities
     def to_dict(self):
         return {
             **super().to_dict(),
